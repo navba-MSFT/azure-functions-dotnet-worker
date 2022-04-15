5314805147347e769bfc60d8b6020893b6a2250c
Shyju Krishnankutty <connectshyju@gmail.com>
2022-04-15 16:55:59 -0700

    Removed ServiceBus from sample proj

--- samples/FunctionApp/ServiceBusTrigger.cs
+++ samples/FunctionApp/ServiceBusTrigger.cs
@@ -1,22 +1,22 @@
-﻿using System;
-using Microsoft.Azure.Functions.Worker;
-using Microsoft.Extensions.Logging;
+﻿//using System;
+//using Microsoft.Azure.Functions.Worker;
+//using Microsoft.Extensions.Logging;
 
-namespace FunctionApp
-{
-    public class ServiceBusTrigger
-    {
-        private readonly ILogger _logger;
+//namespace FunctionApp
+//{
+//    public class ServiceBusTrigger
+//    {
+//        private readonly ILogger _logger;
 
-        public ServiceBusTrigger(ILoggerFactory loggerFactory)
-        {
-            _logger = loggerFactory.CreateLogger<ServiceBusTrigger>();
-        }
+//        public ServiceBusTrigger(ILoggerFactory loggerFactory)
+//        {
+//            _logger = loggerFactory.CreateLogger<ServiceBusTrigger>();
+//        }
 
-        [Function("ServiceBusTrigger")]
-        public void Run([ServiceBusTrigger("myqueue", Connection = "MyServiceBusConnStr")] string myQueueItem)
-        {
-            _logger.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
-        }
-    }
-}
+//        [Function("ServiceBusTrigger")]
+//        public void Run([ServiceBusTrigger("myqueue", Connection = "MyServiceBusConnStr")] string myQueueItem)
+//        {
+//            _logger.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+//        }
+//    }
+//}
